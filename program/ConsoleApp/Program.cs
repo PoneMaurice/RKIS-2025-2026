a8b2d6092752f03c2710fff5809529b242fde1f5
Michael <xmtitovx@gmail.com>
2025-09-20 12:01:00 +0300

    fix

--- program/ConsoleApp/Program.cs
+++ program/ConsoleApp/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 
-namespace ConsApp
+namespace TodoList
 {
     class MainClass
     {
         public static void Main()
         {
             Console.Write("Введите ваше имя: "); 
-            string UserFirstName = Console.ReadLine();
+            var UserFirstName = Console.ReadLine();
             Console.Write("Введите вашу фамилию: ");
-            string UserLastName = Console.ReadLine();
+            var UserLastName = Console.ReadLine();
 
-            short year = 1;
-            byte month = 1;
-            byte day = 1;
+            int year;
+            int month;
+            int day;
 
             do
             {
@@ -25,21 +25,21 @@ namespace ConsApp
             do
             {
                 Console.Write("Введите ваш месяц рождения: ");
-                month = byte.Parse(Console.ReadLine());
+                month = int.Parse(Console.ReadLine());
             }
             while (month > 12 || month < 0);
 
             do
             {
                 Console.Write("Введите ваш день рождения: ");
-                day = byte.Parse(Console.ReadLine());
+                day = int.Parse(Console.ReadLine());
             }
             while (day > 31 || day < 0);
 
             DateTime BirthDate = new DateTime(year, month, day);
-            string UserName = UserFirstName + " " + UserLastName;
+            var UserName = UserFirstName + " " + UserLastName;
 
-            string text = $"\n{DateTime.Now}: добавлен пользователь {UserName}, день рождения {BirthDate.ToLongDateString()}\nвозраст - {DateTime.Now.Year-BirthDate.Year}";
+            var text = $"\n{DateTime.Now}: добавлен пользователь {UserName}, день рождения {BirthDate.ToLongDateString()}\nвозраст - {DateTime.Now.Year-BirthDate.Year}";
 
             Console.WriteLine(text);
         }
