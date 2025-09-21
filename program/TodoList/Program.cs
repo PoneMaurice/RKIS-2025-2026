b7fdf1598bbe95d7b6d81f93195570caa8264dd4
Shevchenko Eduard <ls2618332@gmail.com>
2025-09-21 03:56:57 +0300

    Update Program.cs

--- program/TodoList/Program.cs
+++ program/TodoList/Program.cs
@@ -5,18 +5,16 @@ namespace TodoList
 {
     class MainClass
     {
+        
         public static int survey(string text, int min, int max)
         {
             int result = -1;
             do
             {
                 Console.Write(text);
-                ?string test = Console.ReadLine();
-                Console.WriteLine(test);
                 result = int.Parse(Console.ReadLine());
             }
             while (result < min || result > max);
-
             return result;
         }
         
@@ -29,15 +27,13 @@ namespace TodoList
 
             int OldestPersonYear = 123;
             int NowYear = DateTime.Now.Year;
-
-            // int year = int.Parse(survey("Введите ваш год рождения: ", NowYear - OldestPersonYear, NowYear));
+            
             int year = survey("Введите ваш год рождения: ", NowYear - OldestPersonYear, NowYear);
             int month = survey("Введите ваш месяц рождения: ", 1, 12);
             int day = survey("Введите ваш день рождения: ", 1, 31);
 
             DateTime BirthDate = new DateTime(year, month, day);
             var UserName = UserFirstName + " " + UserLastName;
-
             var text = $"\n{DateTime.Now}: добавлен пользователь {UserName}, день рождения {BirthDate.ToLongDateString()}\nвозраст - {DateTime.Now.Year - BirthDate.Year}";
 
             Console.WriteLine(text);
