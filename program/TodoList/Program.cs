b71bccc5082ad40803eb7aff37293991099cfd5e
Edward <ls2618332@gmail.com>
2025-09-21 03:50:31 +0300

    Изменения Program.cs .gitignor; В Program.cs добавлен новый метод посредством чего он стал более читабельным

--- program/TodoList/Program.cs
+++ program/TodoList/Program.cs
@@ -1,46 +1,44 @@
 ﻿using System;
+using System.Data;
 
 namespace TodoList
 {
     class MainClass
     {
+        public static int survey(string text, int min, int max)
+        {
+            int result = -1;
+            do
+            {
+                Console.Write(text);
+                ?string test = Console.ReadLine();
+                Console.WriteLine(test);
+                result = int.Parse(Console.ReadLine());
+            }
+            while (result < min || result > max);
+
+            return result;
+        }
+        
         public static void Main()
         {
-            Console.Write("Введите ваше имя: "); 
+            Console.Write("Введите ваше имя: ");
             var UserFirstName = Console.ReadLine();
             Console.Write("Введите вашу фамилию: ");
             var UserLastName = Console.ReadLine();
 
-            int year;
-            int month;
-            int day;
             int OldestPersonYear = 123;
+            int NowYear = DateTime.Now.Year;
 
-            do
-            {
-                Console.Write("Введите ваш год рождения: ");
-                year = short.Parse(Console.ReadLine());
-            }
-            while (year < DateTime.Now.Year-OldestPersonYear || year > DateTime.Now.Year);
-
-            do
-            {
-                Console.Write("Введите ваш месяц рождения: ");
-                month = int.Parse(Console.ReadLine());
-            }
-            while (month > 12 || month < 0);
-
-            do
-            {
-                Console.Write("Введите ваш день рождения: ");
-                day = int.Parse(Console.ReadLine());
-            }
-            while (day > 31 || day < 0);
+            // int year = int.Parse(survey("Введите ваш год рождения: ", NowYear - OldestPersonYear, NowYear));
+            int year = survey("Введите ваш год рождения: ", NowYear - OldestPersonYear, NowYear);
+            int month = survey("Введите ваш месяц рождения: ", 1, 12);
+            int day = survey("Введите ваш день рождения: ", 1, 31);
 
             DateTime BirthDate = new DateTime(year, month, day);
             var UserName = UserFirstName + " " + UserLastName;
 
-            var text = $"\n{DateTime.Now}: добавлен пользователь {UserName}, день рождения {BirthDate.ToLongDateString()}\nвозраст - {DateTime.Now.Year-BirthDate.Year}";
+            var text = $"\n{DateTime.Now}: добавлен пользователь {UserName}, день рождения {BirthDate.ToLongDateString()}\nвозраст - {DateTime.Now.Year - BirthDate.Year}";
 
             Console.WriteLine(text);
         }
