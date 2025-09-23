50b5f0441ddabc1f140a0a8815e177f17e0d41b4
Edward <ls2618332@gmail.com>
2025-09-23 16:54:24 +0300

    Хороший обработчик строк

---
+++ Test03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace Task
+{
+    public class Survey
+    {
+        public string str(string text)
+        {
+            Console.Write(text);
+            string? ans = Console.ReadLine();
+            if (ans is null)
+            {
+                return "NULL";
+            }
+            ans = ans.Trim();
+            if (ans == "")
+            {
+                return "NONE";
+            }
+            return ans;
+        }
+    }
+    public static class TaskExtensions
+    {
+        public static void Main()
+        {
+            Survey survey = new Survey();
+            string NameTask = survey.str("Введите название задания: ");
+            string DescTask = survey.str($"Введите описание {NameTask}: ");
+            Console.WriteLine(NameTask);
+            Console.WriteLine(DescTask);
+        }
+    }
+}
